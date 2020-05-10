41ae39db624784f8a397a7f89f860f771168d7cc
ogoshikazuki <ko0701s@gmail.com>
2020-05-10 20:47:48 +0900

    金額修正画面調整

--- resources/views/edit_money.blade.php
+++ resources/views/edit_money.blade.php
@@ -3,27 +3,30 @@
 @section('content')
     <form method="POST" action="{{ route('money.update') }}">
         {{ csrf_field() }}
-        <table class="table">
-            <thead>
-                <tr>
-                    @foreach($players as $player)
-                        <th>{{ $player->name }}</th>
-                    @endforeach
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    @foreach($players as $player)
-                        <td>
-                            <input class="form-control" name="money[{{ $player->id }}]" value="{{ old('money')[$player->id] ?? $currentMoney->moneyPlayer($player)->money }}">
-                        </td>
-                    @endforeach
-                </tr>
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        @foreach($players as $player)
+                            <th>{{ $player->name }}</th>
+                        @endforeach
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        @foreach($players as $player)
+                            <td>
+                                <input class="form-control" name="money[{{ $player->id }}]" value="{{ old('money')[$player->id] ?? $currentMoney->moneyPlayer($player)->money }}">
+                            </td>
+                        @endforeach
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         @error('money')
             <div class="alert alert-danger">{{ $message }}</div>
         @enderror
         <button class="btn btn-primary">更新</button>
+        <a class="btn btn-secondary" href="{{ route('home') }}">戻る</a>
     </form>
 @endsection
