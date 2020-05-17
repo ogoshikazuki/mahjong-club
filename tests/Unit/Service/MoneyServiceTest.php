92a4f3535d8af08f0907114ae5f19ecbc00f8644
ogoshikazuki <ko0701s@gmail.com>
2020-05-17 17:26:55 +0900

    #5 testGetCurrentMoneyPlayers

--- tests/Unit/Service/MoneyServiceTest.php
+++ tests/Unit/Service/MoneyServiceTest.php
@@ -7,8 +7,10 @@ use PHPUnit\Framework\TestCase;
 use Mockery;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
 
 use App\Money;
+use App\MoneyPlayer;
 use App\Service\MoneyService;
 
 class MoneyServiceTest extends TestCase
@@ -36,5 +38,38 @@ class MoneyServiceTest extends TestCase
         $this->assertTrue($actual instanceof Money);
         $this->assertEquals($expect->id, $actual->id);
         $this->assertNull($actual->finished_at);
+    }
+
+    public function testCurrentMoneyPlayers()
+    {
+        $moneyPlayer1 = new MoneyPlayer();
+        $moneyPlayer1->id = 1;
+        $moneyPlayer2 = new MoneyPlayer();
+        $moneyPlayer2->id = 2;
+        $expect = collect([$moneyPlayer1, $moneyPlayer2]);
+
+        $moneyData = new Money();
+        $moneyData->moneyPlayers = $expect;
+
+        $builder = Mockery::mock(Builder::class)
+            ->shouldReceive('firstOrFail')
+            ->andReturn($moneyData)
+            ->getMock();
+
+        $money = Mockery::mock(Money::class)
+            ->shouldReceive('whereNull')
+            ->with('finished_at')
+            ->andReturn($builder)
+            ->getMock();
+
+        $moneyService = new MoneyService($money);
+
+        $actual = $moneyService->getCurrentMoneyPlayers();
+
+        $this->assertTrue($actual instanceof Collection);
+        for ($index = 0; $index < 2; $index++) {
+            $this->assertTrue($actual->get($index) instanceof MoneyPlayer);
+            $this->assertEquals($expect->get($index)->id, $actual->get($index)->id);
+        }
     }
 }
