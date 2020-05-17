d3d4d4ca1bf34ce9e05fb995bf0a53aefe716b4c
ogoshikazuki <ko0701s@gmail.com>
2020-05-17 16:38:37 +0900

    #5 assert instance

--- tests/Unit/Service/PlayerServiceTest.php
+++ tests/Unit/Service/PlayerServiceTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+use Illuminate\Support\Collection;
+
 use Mockery;
 
 use App\Service\PlayerService;
@@ -21,12 +23,13 @@ class PlayerServiceTest extends TestCase
             ->shouldReceive('all')
             ->andReturn($expect)
             ->getMock();
-
         $playerService = new PlayerService($player);
 
         $actual = $playerService->getAllPlayers();
 
+        $this->assertTrue($actual instanceof Collection);
         for ($index = 0; $index <= 1; $index++) {
+            $this->assertTrue($actual->get($index) instanceof Player);
             $this->assertEquals($expect->get($index)->id, $actual->get($index)->id);
             $this->assertEquals($expect->get($index)->name, $actual->get($index)->name);
         }
