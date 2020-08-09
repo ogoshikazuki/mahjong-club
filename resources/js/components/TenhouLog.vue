4bd45b95168b3e2a0cd80d56a0b53bed5e8f8ec9
ogoshikazuki <ko0701s@gmail.com>
2020-08-09 13:58:10 +0900

    #44

--- resources/js/components/TenhouLog.vue
+++ resources/js/components/TenhouLog.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <v-alert type="warning">天鳳ログは自動取得しているので、原則ここからは登録しないでね。</v-alert>
     <v-alert v-if="registeredCount" type="success">{{ registeredCount }}件を登録しました。</v-alert>
     <tenhou-log-downloader @complete="tenhouLogs = $event"></tenhou-log-downloader>
     <tenhou-log-registerer v-if="tenhouLogs.length > 0" :tenhou-logs="tenhouLogs" @registered="registered($event)"></tenhou-log-registerer>
