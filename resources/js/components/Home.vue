0068c44ec691a3426124a6d69959eefaebfb3384
ogoshikazuki <ko0701s@gmail.com>
2021-01-02 17:23:42 +0900

    feature: SuggestResetMoneyDialog

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -6,6 +6,7 @@
       <v-card-actions>
         <v-btn color="warning" :to="{ name: 'edit-money' }">金額修正</v-btn>
         <v-btn color="secondary" :to="{ name: 'history' }">履歴表示</v-btn>
+        <v-btn color="purple" dark @click="suggestResetMoneyDialog = true">精算提案</v-btn>
         <v-btn color="primary" @click="resetMoney">精算</v-btn>
       </v-card-actions>
     </v-card>
@@ -13,23 +14,27 @@
       <v-card-title>精算済</v-card-title>
       <PastMoney></PastMoney>
     </v-card>
+    <SuggestResetMoneyDialog v-model="suggestResetMoneyDialog"></SuggestResetMoneyDialog>
   </div>
 </template>
 
 <script>
 import CurrentMoney from "./CurrentMoney";
 import PastMoney from "./PastMoney";
+import SuggestResetMoneyDialog from "./SuggestResetMoneyDialog";
 import ApiClient from "../ApiClient";
 
 export default {
   components: {
     CurrentMoney,
     PastMoney,
+    SuggestResetMoneyDialog,
   },
 
   data() {
     return {
       resetting: false,
+      suggestResetMoneyDialog: false,
     };
   },
 
---
+++ resources/js/components/SuggestResetMoneyDialog.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-dialog :value="value" @input="$emit('input', $event)" v-loading="loading">
+    <v-card>
+      <v-card-title>精算提案</v-card-title>
+      <v-card-text>
+        <v-simple-table class="text-no-wrap">
+          <tbody>
+            <tr v-for="(suggest, i) in suggest1" :key="i">
+              <td>{{ suggest.from }}</td>
+              <td><v-icon>mdi-arrow-right</v-icon></td>
+              <td>{{ suggest.to }}</td>
+              <td>{{ suggest.money }}</td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import ApiClient from "../ApiClient";
+
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    }
+  },
+
+  data() {
+    return {
+      moneyPlayers: [],
+      loading: true,
+    }
+  },
+
+  computed: {
+    minusMoneyPlayers() {
+      return [...this.moneyPlayers]
+        .sort((a, b) => a.money - b.money)
+        .filter(({ money }) => money < 0);
+    },
+
+    plusMoneyPlayers() {
+      return [...this.moneyPlayers]
+        .sort((a, b) => b.money - a.money)
+        .filter(({ money }) => money > 0);
+    },
+
+    suggest1() {
+      if (this.loading) {
+        return [];
+      }
+
+      const result = [];
+
+      let minusIndex = 0;
+      let plusIndex = 0;
+      let minus = - this.minusMoneyPlayers[minusIndex].money;
+      let plus = this.plusMoneyPlayers[plusIndex].money;
+
+      minus: while (minusIndex < this.minusMoneyPlayers.length) {
+        plus: while (plusIndex < this.plusMoneyPlayers.length) {
+          if (plus < minus) {
+            result.push({
+              from: this.minusMoneyPlayers[minusIndex].player.name,
+              to: this.plusMoneyPlayers[plusIndex].player.name,
+              money: plus,
+            });
+            minus -= plus;
+            plusIndex++;
+            plus = this.plusMoneyPlayers[plusIndex]?.money;
+            continue plus;
+          }
+          result.push({
+            from: this.minusMoneyPlayers[minusIndex].player.name,
+            to: this.plusMoneyPlayers[plusIndex].player.name,
+            money: minus,
+          });
+          plus -= minus;
+          minusIndex++;
+          minus = - this.minusMoneyPlayers[minusIndex]?.money;
+          continue minus;
+        }
+      }
+
+      return result;
+    },
+  },
+
+  async created() {
+    this.moneyPlayers = (await ApiClient.getCurrentMoney()).money_players;
+    this.loading = false;
+  },
+}
+</script>
