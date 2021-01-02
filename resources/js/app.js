245534dcdf2525f66c58a4a2444313418aabe29f
ogoshikazuki <ko0701s@gmail.com>
2021-01-02 19:49:00 +0900

    refactor: remove elementui locale

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,9 +25,8 @@ const Vue = require("vue");
 
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
-import locale from "element-ui/lib/locale/lang/ja";
 
-Vue.use(ElementUI, { locale });
+Vue.use(ElementUI);
 
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
